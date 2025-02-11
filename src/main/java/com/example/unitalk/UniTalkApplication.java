277fc60ba32ce307093f3b92836be83eb4e707c5
netzus1 <netzus@protonmail.com>
2025-02-11 10:17:02 +0100

    Commit inicial con el nuevo proyecto

---
+++ src/main/java/com/example/unitalk/UniTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.unitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unitalk/UniTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unitalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
