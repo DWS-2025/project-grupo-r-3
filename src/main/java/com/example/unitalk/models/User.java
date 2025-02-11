6b73658eacbdd13dd6ef17652549bb39b9072a13
p4b4al <pablobardonalonso@gmail.com>
2025-02-11 10:55:21 +0100

    create user class and service

---
+++ src/main/java/com/example/unitalk/models/User.java
@@ -0,0 +1,13 @@
+package com.example.unitalk.models;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+
+    public User(int id, String username, String email){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/unitalk/services/UserService.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import com.example.unitalk.models.User;
+
+package com.example.unitalk.services;
+
+
+
+@Service
+public class UserService {
+    public void newUser(Long id, String username, String email){}
+    public User getUserById(Long id){
+        User.
+    }
+    public User getUserByUsername(String username){}
+    public void deleteUser(Long id){}
+    public void modifyUser(Long id){}
+}
