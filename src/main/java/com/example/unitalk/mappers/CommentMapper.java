f1eddbad7c72a90c7c79a8b7a1fc9652f2ce6141
netzus1 <netzus@protonmail.com>
2025-04-07 21:33:49 +0200

    Added mapper functionality

--- src/main/java/com/example/unitalk/mappers/CommentMapper.java
+++ src/main/java/com/example/unitalk/mappers/CommentMapper.java
@@ -2,16 +2,23 @@ package com.example.unitalk.mappers;
 
 import com.example.unitalk.DTOS.CommentDTO;
 import com.example.unitalk.DTOS.CommentInputDTO;
+import com.example.unitalk.DTOS.CommentRestDTO;
 import com.example.unitalk.models.Comment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
 
     CommentDTO toDTO(Comment comment);
-
     List<CommentDTO> toDTOs(List<Comment> comments);
-
     Comment toEntity(CommentInputDTO commentDTO);
+
+    @Mapping(target = "userId", expression = "java(comment.getUser() != null ? comment.getUser().getId() : null)")
+    @Mapping(target = "postId", expression = "java(comment.getPost() != null ? comment.getPost().getId() : null)")
+    CommentRestDTO toRestDTO(Comment comment);
+
+    List<CommentRestDTO> toRestDTOs(List<Comment> comments);
 }
--- src/main/java/com/example/unitalk/mappers/PostMapper.java
+++ src/main/java/com/example/unitalk/mappers/PostMapper.java
@@ -1,19 +1,29 @@
 package com.example.unitalk.mappers;
-
+import com.example.unitalk.DTOS.PostRestDTO;
 import com.example.unitalk.DTOS.PostDTO;
 import com.example.unitalk.DTOS.PostInputDTO;
 import com.example.unitalk.models.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
     PostDTO toDTO(Post post);
-
     List<PostDTO> toDTOs(List<Post> posts);
     Post toEntity(PostDTO postDTO);
     Post toEntity(PostInputDTO postDTO);
 
+    // Methods for rest
+    @Mapping(target = "subjectId", expression = "java(post.getSubject() != null ? post.getSubject().getId() : null)")
+    @Mapping(target = "userId", expression = "java(post.getUser() != null ? post.getUser().getId() : null)")
+    PostRestDTO toRestDTO(Post post);
+    List<PostRestDTO> toRestDTOs(List<Post> posts);
+
+
+    @Mapping(target = "subject", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "comments", ignore = true)
+    Post toEntity(PostRestDTO postRestDTO);
 }
